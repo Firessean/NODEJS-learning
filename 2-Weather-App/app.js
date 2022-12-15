14cad54bb871e4257bb66827fbfce5dd161cfc7c
Firessean <themrsaus@gmail.com>
2022-12-16 01:32:28 +0200

    Installed request npm, and learned how get access to some data by using HTTP Request

--- 2-Weather-App/app.js
+++ 2-Weather-App/app.js
@@ -1,12 +1,10 @@
 'use strict';
-console.log('Starting');
+const request = require('request');
 
-setTimeout(() => {
-  console.log('2 Second Timer');
-}, 2000);
+const url =
+  'http://api.weatherstack.com/current?access_key=07f18234a8b3fd4528a8b69493b4d875&query=37.8267,-122.4233';
 
-setTimeout(() => {
-  console.log('0 Second Timer');
-}, 0);
-
-console.log('Stopping');
+request({ url: url }, (error, response) => {
+  const data = JSON.parse(response.body);
+  console.log(data.current);
+});
