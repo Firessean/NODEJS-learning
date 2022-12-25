f958e38425e0aeee3ab278d6627d10db02dbfe9d
Firessean <themrsaus@gmail.com>
2022-12-25 19:59:41 +0200

    Used callback chaining for chain geocode and forecast functions, used argv for getting location from users.

--- 2-Weather-App/app.js
+++ 2-Weather-App/app.js
@@ -2,12 +2,22 @@
 const geocode = require('./Utils/geocode');
 const forecast = require('./Utils/forecast');
 
-geocode('Dnipro Ukraine', (error, data) => {
-  console.log('Error', error);
-  console.log('Data', data);
-});
+const address = process.argv[2];
+if (!address) {
+  console.log('Please provide an address');
+} else {
+  geocode(address, (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
 
-forecast(44.1545, -75.7088, (error, data) => {
-  console.log('Error', error);
-  console.log('Data', data);
-});
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+
+      console.log(data.location);
+      console.log(forecastData);
+    });
+  });
+}
