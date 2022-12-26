8f11d8baf5e28bcf55fb4c4b2ecdf63dffa475e7
Firessean <themrsaus@gmail.com>
2022-12-26 02:58:48 +0200

    Replaced nested destructuring object with more simple destructuring for determinate bug when we expect error message (Unable to connect to geocoding service!)

--- 2-Weather-App/Utils/geocode.js
+++ 2-Weather-App/Utils/geocode.js
@@ -6,38 +6,22 @@ const geocode = (address, callback) => {
     address
   )}.json?access_token=pk.eyJ1IjoiZmlyZXNzZWFuIiwiYSI6ImNsYnh1YmZpajAxaHYzdHBlbWFldHltYWMifQ.oYVL6V_X1p7DM4OuJ9sHeg&limit=1`;
 
-  request(
-    { url, json: true },
-    (
-      error,
-      {
-        body: {
-          features: [
-            {
-              place_name: location,
-              center: [longitude, latitude],
-            },
-          ],
-          features,
-        },
-      }
-    ) => {
-      if (error) {
-        callback('Unable to connect to geocoding service!', undefined);
-      } else if (features.length === 0) {
-        callback(
-          'Unable to find location! Try again with different search term',
-          undefined
-        );
-      } else {
-        callback(undefined, {
-          latitude,
-          longitude,
-          location,
-        });
-      }
+  request({ url, json: true }, (error, { body } = {}) => {
+    if (error) {
+      callback('Unable to connect to geocoding service!', undefined);
+    } else if (body.features.length === 0) {
+      callback(
+        'Unable to find location! Try again with different search term',
+        undefined
+      );
+    } else {
+      callback(undefined, {
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name,
+      });
     }
-  );
+  });
 };
 
 module.exports = geocode;
