24c1f31778faeb3253b53caaca9b59945dff0e87
Firessean <themrsaus@gmail.com>
2022-12-27 14:41:28 +0200

    Learned how to create local web-server using Express Library

---
+++ web-server/src/app.js
@@ -0,0 +1,28 @@
+'use strict';
+const express = require('express');
+
+const app = express();
+
+app.get('', (req, res) => {
+  res.send('Hello Express!');
+});
+
+app.get('/help', (req, res) => {
+  res.send('Help page');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About page');
+});
+
+app.get('/weather', (req, res) => {
+  res.send('Get the weather');
+});
+
+// app.com
+// app.com/help
+// app.com/about
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000.');
+});
