1d3ef765139042a5e7f762dfeb377908f6ed858f
Firessean <themrsaus@gmail.com>
2022-12-27 15:00:26 +0200

    Learned how send back HTML and JSON data

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,24 +4,31 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-  res.send('Hello Express!');
+  res.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (req, res) => {
-  res.send('Help page');
+  res.send([
+    {
+      name: 'Dmytro',
+      age: 24,
+    },
+    {
+      name: 'Sarah',
+    },
+  ]);
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page');
+  res.send('<h1>About</h1>');
 });
 
 app.get('/weather', (req, res) => {
-  res.send('Get the weather');
+  res.send({
+    forecast: '5 degrees out',
+    location: 'Dnipro Ukraine',
+  });
 });
-
-// app.com
-// app.com/help
-// app.com/about
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000.');
