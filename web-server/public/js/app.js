23200ac8b077939ef81a651b0d7c60485442f8b1
Firessean <themrsaus@gmail.com>
2023-01-19 01:10:44 +0200

    Learned about browser HTTP requests with fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,2 +1,11 @@
 'use strict';
 console.log('Client side javascript is loaded!');
+
+fetch('http://localhost:3000/weather?address=Boston').then((response) => {
+  response.json().then((data) => {
+    if (data.error) return console.log(data.error);
+
+    console.log(data.location);
+    console.log(data.forecast);
+  });
+});
