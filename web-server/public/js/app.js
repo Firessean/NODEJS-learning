41a2cecc2c0a60c6a061dfa4a935d31a57eea23f
Firessean <themrsaus@gmail.com>
2023-01-19 12:45:57 +0200

    Created a search form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,11 +1,21 @@
 'use strict';
 console.log('Client side javascript is loaded!');
 
-fetch('http://localhost:3000/weather?address=Boston').then((response) => {
-  response.json().then((data) => {
-    if (data.error) return console.log(data.error);
+const weatherForm = document.querySelector('form');
+const searchElement = document.querySelector('input');
 
-    console.log(data.location);
-    console.log(data.forecast);
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const location = searchElement.value;
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) return console.log(data.error);
+
+      console.log(data.location);
+      console.log(data.forecast);
+    });
+
   });
 });
